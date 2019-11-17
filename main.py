5356d8bd22262ed3cfa64ce3093c9790b9a7915b
juntanaka <edamame.zombie@gmail.com>
2019-11-17 18:04:18 +0900

    メッセージ変更2

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text="おっぱい"))
+                               TextSendMessage(text='おっぱい'))  # text=event.message.text ← オウム返し
 
 
 if __name__ == '__main__':
