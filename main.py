70d49d33960fc46bbdfca044955b09623dcfa4fb
juntanaka <edamame.zombie@gmail.com>
2019-11-17 18:03:04 +0900

    メッセージ変更2

--- main.py
+++ main.py
@@ -39,10 +39,12 @@ def callback():
     return 'OK'
 
 
+# https://qiita.com/mikan3rd/items/0e1bfe46e00cf434a126
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,
-                               TextSendMessage(text=event.message.text))
+                               TextSendMessage(text="おっぱい"))
 
 
 if __name__ == '__main__':
